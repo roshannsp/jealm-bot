b571bc847691a67eaa4c6c251be59af666b789a0
9terz <9terz666@gmail.com>
2020-09-13 01:28:58 +0700

    improve performance

--- src/realm.ts
+++ src/realm.ts
@@ -1,8 +1,10 @@
 import { AxiosResponse, AxiosStatic } from 'axios'
 import { API } from './constants'
+import fs from 'fs';
 
+type PlayerInfo = { [key: string]: Promise<any> }
 export class Realm {
-  constructor(private readonly httpClient: AxiosStatic) {}
+  constructor(private readonly httpClient: AxiosStatic) { }
 
   async available() {
     const res = await this.httpClient.get(API.REALM + '/mco/available')
@@ -19,19 +21,33 @@ export class Realm {
     const world = worlds.find((world: any) => world.name === name)
     const serverList = await this.liveplayerlist()
     const server = serverList.find((server: any) => (server.id = world.id))
-    let player: string[] = []
+    let players: string[] = []
+    const cacheString = fs.readFileSync('src/cache/uuid.json').toString()
+    const cache = JSON.parse(cacheString);
     if (server) {
       const playerList = JSON.parse(server.playerList)
-      const promises = playerList.map(async (player: any) => {
+      const playerInfoList: PlayerInfo[] = playerList.map((player: any) => {
+        if (cache[player.playerId] !== undefined) {
+          return { [player.playerId]: cache[player.playerId] }
+        }
         const api = `/user/profiles/${player.playerId}/names`
-        return this.httpClient.get(API.MOJANG + api)
+        return {
+          [player.playerId]: this.httpClient.get(API.MOJANG + api)
+        }
       })
-      const result = await Promise.all(promises)
-      result.forEach((r: any) => {
-        player.push(r.data[r.data.length - 1].name)
-      })
+      for await (const playerInfo of playerInfoList) {
+        const promise = Object.values(playerInfo)[0];
+        const res = await promise;
+        const playerID = Object.keys(playerInfo)[0]
+        const playerName = res.data ? res.data[res.data.length - 1].name : res
+        if (cache[playerID] === undefined) {
+          cache[playerID] = playerName;
+        }
+        players.push(playerName)
+      }
     }
-    return player || []
+    await fs.writeFileSync('src/cache/uuid.json', JSON.stringify(cache))
+    return players || []
   }
 
   async version() {
